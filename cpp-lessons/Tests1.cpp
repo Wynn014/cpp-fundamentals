3c8db589484a0e75556361fe9e786b1b049e9665
Wynn014 <ayhonzen@gmail.com>
2026-06-24 21:49:05 +0800

    style: batch rename single-digit lessons with leading zeros

--- cpp-lessons/Tests1.cpp
+++ cpp-lessons/Tests1.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 
-namespace first{
-    int x = 1;
-}
-namespace second{
-    int x = 2;
-}
-namespace first{
-    int y = 5;
-}
-namespace second{
-    int y = 6;
-}
-
 int main() {
-    const int day = 79;
-    std::string word = "hello";
-    char grade = 'A';
+    int age;
+    int day; // Keeps 'day' as a number (1 = Monday, etc.)
 
-    std::cout << word << " and " << day << '\n';
-    std::cout << "My grade is " << grade << '\n';
-    std::cout << "this is number " <<  first::x << std::endl;
-    std::cout << "we have " << first::x << " cows and " << second::x << " pieces of bread" << '\n';
-    std::cout << "I also have " << second::y << " apples and " << first::y << " pieces of orange" << '\n';
+    std::cout << "Enter the day number (1-7): ";
+    std::cin >> day;
+
+    // 1. Check the day first
+    switch (day) {
+        case 1:
+            std::cout << "It is Monday!\n";
+            break;
+        case 2:
+             std::cout << "It is thursday!\n";
+            break;
+        case 3:
+             std::cout << "It is Wednesday\n";
+             break;
+        default:
+            std::cout << "Aint even a day\n";
+            break;
+        
+    }
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    // 2. Then check the age gate
+    if (age >= 18) {
+        std::cout << "Welcome to the site\n";
+    } 
+    else if (age <= 0) {
+        std::cout << "Invalid age\n";
+    } 
+    else {
+        std::cout << "You are not allowed\n";
+    }
 
     return 0;
 }
