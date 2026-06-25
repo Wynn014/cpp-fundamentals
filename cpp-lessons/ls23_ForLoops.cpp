0e8a7a3f15c0e102b98e74388f316f74c044c93a
Wynn014 <ayhonzen@gmail.com>
2026-06-25 23:53:10 +0800

    Update for loop lesson to count down from 10 to 0 with sleep delay

--- cpp-lessons/ls23_ForLoops.cpp
+++ cpp-lessons/ls23_ForLoops.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
     // 1. Declare counter index; 2. Set test condition; 3. Increment counter
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 10; i >= 0; i--) {
         std::cout << "Iteration: " << i << "\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
-
+  //configured the speed of intervals
+  //made it from 1-10 to 10-0; basically just reversed it
     std::cout << "Happy New Year!\n";
 
     return 0;
